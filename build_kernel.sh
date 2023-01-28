ebf811bbfe92310aa6810a504fa5876b43263f62
DY3413 <milosix3413@gmail.com>
2023-01-28 16:27:21 +0900

    Fix build error

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,6 +6,6 @@ export CROSS_COMPILE=$(pwd)/arm-eabi-4.8/bin/arm-eabi-
 mkdir output
 
 make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_grandmax_koropen_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=output
+make -j7 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
